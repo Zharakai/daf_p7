d5ffb626c07ad51d955af98e999ee38110b21575
unknown <yannis.almanric@gmail.com>
2021-09-03 14:51:32 +0200

    Initial commit, project created with Vue CLI

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: ['plugin:vue/essential', 'airbnb-base'],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {},
+  settings: {
+    'import/resolver': {
+      alias: {
+        extensions: ['.js', '.vue', '.json'],
+        map: [
+          ['~', path.resolve(__dirname, './')],
+          ['@', path.resolve(__dirname, './')],
+        ],
+      },
+    },
+  },
+};
+
+/*
 module.exports = {
   root: true,
   env: {
@@ -15,3 +44,4 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
 };
+*/
