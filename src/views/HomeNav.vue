b7ffd8cbdd0278d56e57f1a23acc0d389f5e6377
unknown <yannis.almanric@gmail.com>
2021-11-12 11:06:56 +0100

    New views

---
+++ src/views/HomeNav.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="nav" class="homeNav">
+    <img alt="Just Here logo" src="../assets/logo.png">
+    <div class="restaurantsNav">
+      <label for="ratingsFilter">Filtre par notes</label>
+      <select name="filter" id="ratingsFilter">
+        <option value="allRatings">Toutes les notes</option>
+        <option value="2">2</option>
+        <option value="2.5">2.5</option>
+        <option value="3">3</option>
+        <option value="3.5">3.5</option>
+        <option value="4">4</option>
+        <option value="4.5">4.5</option>
+      </select>
+      <ul>
+        <li
+          v-for="restaurant in restaurantsList"
+          :key="restaurant.restaurantName">
+          <router-link :to="{ name: 'Restaurant', params: { id: restaurant.restaurantName }}">
+            <div>
+              <p>{{ restaurant.restaurantName }}</p>
+              <p>{{ getAverageRating(restaurant.ratings) }}</p>
+              <p>{{ restaurant.address }}</p>
+            </div>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useStore } from 'vuex';
+
+export default {
+  setup() {
+    const store = useStore();
+    const { restaurantsList } = store.state;
+
+    function getAverageRating(ratings) {
+      // Create an array for each restaurant with their ratings
+      const flatRatings = ratings.map((rating) => rating.stars);
+      // Take each array previously created to return the average with one decimal
+      return Math.round((flatRatings.reduce((a, b) => a + b) / ratings.length) * 10) / 10;
+    }
+
+    return {
+      restaurantsList,
+      getAverageRating,
+    };
+  },
+};
+</script>
---
+++ src/views/Restaurant.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="selectedRestaurant hidden">
+    <p class="back" aria-label="Retour" title="Retour"></p>
+    <div>
+        <p>img</p>
+        <p>restaurantName</p>
+        <p>averageRating</p>
+        <p>address</p>
+        <p>comments</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.back {
+    width: fit-content;
+
+    &:hover {
+      cursor: pointer;
+      color: #42b983;
+    }
+
+    &::before {
+    content: '\1F810';
+    font-size: 3rem;
+  }
+  }
+</style>
